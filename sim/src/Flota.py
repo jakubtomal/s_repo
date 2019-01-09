5f2ffb944a15cab47e61c1b352e3a6e45f93e21a
dredi <dredi@localhost>
2019-01-09 17:45:44 +0100

    flota - powinna działać

--- sim/src/Flota.py
+++ sim/src/Flota.py
@@ -4,26 +4,30 @@ Created on 19 lis 2018
 @author: dredi
 '''
 class Flota():
-    def __init__(self):
-        self.mt = 0
-        self.dt = 0
-        self.lm = 0
-        self.cm = 0
-        self.kr = 0
-        self.ow = 0
-        self.sk = 0
-        self.re = 0
-        self.ss = 0
-        self.bb = 0
-        self.ns = 0
-        self.gs = 0
-        self.pa = 0
-        
-    def czytaj_flote(self,nazwa_pliku):
+    def __init__(self , nazwa_pliku):
         plik = open(nazwa_pliku , 'r')
         tmplist = []
         
         for i in plik:
             tmplist.append(i[3:-1])
+            
+        plik.close()
+            
+        self.mt = tmplist[0]
+        self.dt = tmplist[1]
+        self.lm = tmplist[2]
+        self.cm = tmplist[3]
+        self.kr = tmplist[4]
+        self.ow = tmplist[5]
+        self.sk = tmplist[6]
+        self.re = tmplist[7]
+        self.ss = tmplist[8]
+        self.bb = tmplist[9]
+        self.ns = tmplist[10]
+        self.gs = tmplist[11]
+        self.pa = tmplist[12]
+        
+        
+
             
         
