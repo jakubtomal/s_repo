7f68794dad543764ea5e4ebaf96242da5e768e9a
dredi <dredi@localhost>
2019-01-13 00:53:26 +0100

    dzialajaca klasa statek z dodanymi komentarzami

--- sim/src/Statek.py
+++ sim/src/Statek.py
@@ -1,6 +1,8 @@
 
 from random import randint
+from Dane_statkow import dane_statkow
 
+statek_info = dane_statkow();
 plik_sd = open('szybkie_dziala.txt' , 'r' )
 lista_sd = []
 
@@ -11,33 +13,66 @@ for i in plik_sd:
 
 class statek():
     def __init__(self ,  Tab):
+        '''klasa statek : 
+        name(str): skrot nazwy statku, 
+        pancerz(float): aktualny pancerz statku,
+        pancerz_max(float): pancerz nie uszkodzonego statku
+        oslona(float): aktualna oslona statku
+        oslona_max(float): oslona nie uszkodzonego statku
+        atak(float): obrazenia jakie zadaje statek 
+        '''
         self.name = Tab[0]
         self.pancerz = float(Tab[1])
+        self.pancerz_max = float(Tab[1])
         self.oslona = float(Tab[2])
+        self.oslona_max = float(Tab[2])
         self.atak = float(Tab[3])
         
     def strzel(self,statek2):
+        ''' Funkcja sprawdzająca czy statek będzie mogl ponownie strzelic  '''
         
         statek_dziala = {'mt' : 0 , 'dt' : 1 , 'lm' : 2 
                          , 'cm' : 3 , 'kr' : 4 , 'ow' : 5 
                          , 'sk' : 6 , 're' : 7 , 'ss' : 8 
                          , 'bb' : 9 , 'ns' : 10 , 'gs' : 11 
-                         , 'pa' : 12 }
+                         , 'pa' : 12 } # slownik przypisujacy nazwie statku odpowiedni indeks w tablicy szybkich dzial
         
-        szansa = 100 * (1 - 1/float(lista_sd[statek_dziala[self.name]][statek_dziala[statek2.name]]))
+        szansa = 100 * (1 - 1/float(lista_sd[statek_dziala[self.name]][statek_dziala[statek2.name]])) # szansa na to ze statek bedzie mogl oddac ponowny strzal
         
         if randint(0,100) <= szansa:
-            return True
+            return True #statek moze ponownie strzelic
         else:
-            return False
+            return False # statek nie moze ponownie strzelac
         
     def trafiony(self , statek2):
-        if ( statek2.atak / self.oslona) < 0.1:
-            return False
-        else:
-            #if self.pancerz/
-            return True
+        ''' Funkcja obslugujaca co sie stanie jezeli w statek strzeli statek2'''
         
+        if self.oslona > 0:
+            if ( statek2.atak / self.oslona) < 0.1:
+                print("pudlo")
+                return False #statek2 nie trafil
+        
+        obrazenia = statek2.atak
+        oslona_tmp = self.oslona
+        self.oslona -= obrazenia
+        obrazenia -= oslona_tmp
+        if self.oslona < 0:
+            self.oslona = 0
+        if obrazenia > 0:
+            self.pancerz -= obrazenia;
+            if self.pancerz <= 0:
+                self.pancerz = 0
+                
+        if self.pancerz < (self.pancerz_max * 0.7): 
+            szansa_wybuch = 100 *( 1 - (self.pancerz / self.pancerz_max)) #szansa na to ze statek wybuchnie
+            if randint(0,100) <= szansa_wybuch:
+                self.pancerz = 0
+                return True #statek zostal zniszczony
+            
+            else:
+                return False #statek nie zostal zniszczony
+        else:
+            return False #statek nie zostal zniszczony
         
         
         
