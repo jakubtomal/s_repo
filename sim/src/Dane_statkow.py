7431790fdff2e589b7e83a32dfa2e7d63651ce42
dredi <dredi@localhost>
2019-01-13 01:15:21 +0100

    tablice w klasie dane statkow zmienione na statyczne

--- sim/src/Dane_statkow.py
+++ sim/src/Dane_statkow.py
@@ -1,27 +1,24 @@
 
 class dane_statkow:
-    def __init__(self):
         
-        tmplist=[]
-        file1 = open('Dane_statkow.txt' , 'r')
-        for i in file1:
-            tmplist.append(i[:-1].split(','))
-        file1.close()
-            
-        self.mt = tmplist[0]
-        self.dt = tmplist[1]
-        self.lm = tmplist[2]
-        self.cm = tmplist[3]
-        self.kr = tmplist[4]
-        self.ow = tmplist[5]
-        self.sk = tmplist[6]
-        self.re = tmplist[7]
-        self.ss = tmplist[8]
-        self.bb = tmplist[9]
-        self.ns = tmplist[10]
-        self.gs = tmplist[11]
-        self.pa = tmplist[12]
+    tmplist=[]
+    file1 = open('Dane_statkow.txt' , 'r')
+    for i in file1:
+        tmplist.append(i[:-1].split(','))
+    file1.close()
         
-        
+    mt = tmplist[0]
+    dt = tmplist[1]
+    lm = tmplist[2]
+    cm = tmplist[3]
+    kr = tmplist[4]
+    ow = tmplist[5]
+    sk = tmplist[6]
+    re = tmplist[7]
+    ss = tmplist[8]
+    bb = tmplist[9]
+    ns = tmplist[10]
+    gs = tmplist[11]
+    pa = tmplist[12]
         
 
